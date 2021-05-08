6eddc233d880e4fb449e3d0a7cb986dbcb62f00c
Vladimir Ivanov <vla.a.ivanov@tinkoff.ru>
2021-05-08 16:30:36 +0500

    add routing

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,4 +1,5 @@
 import React, { FunctionComponent } from 'react';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 import { Header } from '../Header/Header';
 import classes from './App.module.scss';
 import HomeIcon from '../../icons/home.svg';
@@ -6,10 +7,16 @@ import { Example } from '../../containers/Example';
 
 export const App: FunctionComponent = () => {
   return (
-    <div className={classes.main}>
-      <HomeIcon />
-      <Header text="Hello" />
-      <Example />
-    </div>
+    <Router>
+      <div className={classes.main}>
+        <HomeIcon />
+        <Header text="Hello" />
+        <Switch>
+          <Route path="/">
+            <Example />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 };
