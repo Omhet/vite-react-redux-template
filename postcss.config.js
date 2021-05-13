faf4d560e256e0e041ec1eabf080afa28f514266
Vladimir Ivanov <omhetehmo@yandex.ru>
2021-05-13 23:13:53 +0500

    Turn in postcss

--- postcss.config.js
+++ postcss.config.js
@@ -1,11 +1,6 @@
 module.exports = () => {
   return {
     parser: 'postcss-scss',
-    plugins: [
-      require('postcss-preset-env')(),
-      require('postcss-nested')(),
-      require('postcss-sort-media-queries')(),
-      require('autoprefixer')(),
-    ],
+    plugins: [require('postcss-preset-env')(), require('autoprefixer')()],
   };
 };
