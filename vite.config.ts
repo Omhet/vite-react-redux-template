e2e840c2d273d372487f4972e6785a43c8e205de
Vladimir Ivanov <omhetehmo@yandex.ru>
2021-05-14 11:58:30 +0500

    Generate css class name

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,20 @@ import reactRefresh from '@vitejs/plugin-react-refresh';
 import reactSvgPlugin from 'vite-plugin-react-svg';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    reactRefresh(),
-    reactSvgPlugin({
-      defaultExport: 'component',
-    }),
-  ],
-});
+export default ({ command }) => {
+  const isDev = command === 'serve';
+
+  return defineConfig({
+    css: {
+      modules: {
+        generateScopedName: isDev ? '[name]__[local]___[hash:base64:5]' : undefined,
+      },
+    },
+    plugins: [
+      reactRefresh(),
+      reactSvgPlugin({
+        defaultExport: 'component',
+      }),
+    ],
+  });
+};
